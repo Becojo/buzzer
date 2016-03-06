abb3f88dbd8428ac7dc4fbb8bbb0ae412cdaac6a
Benoit Cote-Jodoin <benoit.cote.jodoin@gmail.com>
2016-03-06 14:03:07 -0500

    set leds to outputs and added serial  communication

--- buzzer.ino
+++ buzzer.ino
@@ -1,14 +1,18 @@
 byte COUNT = 3;
 byte INPUTS[] = {2, 3, 4};
 byte LEDS[] = {9, 11, 10};
+String NAMES[] = {"red", "green", "blue"};
 byte RESET_PIN = 5;
 
 byte state = 0;
 bool flag = true;
 
 void setup() {
+  Serial.begin(115200);
+  
   for(int i = 0; i < COUNT; i++) {
     pinMode(INPUTS[i], INPUT_PULLUP);  
+    pinMode(LEDS[i], OUTPUT);
   }
   
   pinMode(RESET_PIN, INPUT_PULLUP);
@@ -26,6 +30,9 @@ void loop() {
       if(digitalRead(INPUTS[i]) == LOW) {
         flag = false;
         state = i;
+
+        Serial.print("Team: ");
+        Serial.println(NAMES[state]);
       }
     }
   } else {
